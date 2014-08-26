ae67917049f38aa341ad2cf215ef5eac85492a7f
Oscar Gutierrez L <ogutierrez@transcomer.com>
2014-08-26 17:25:37 -0600

    Actualiza nuevo_modulo con teachers

--- nuevo_modulo/nuevo_modulo.py
+++ nuevo_modulo/nuevo_modulo.py
@@ -8,6 +8,9 @@ class Course (osv.Model):
         'name': fields.char('Name', size=128, required=True, select=True),
         'code': fields.char('Code', size=32, required=True, select=True),
         'description': fields.text('Description'),
+        'session_ids': fields.one2many('nuevo_modulo.session','course_id',string='Sessions'),
+        'student_ids': fields.many2many('nuevo_modulo.student',string="Students"),
+        'teacher_id': fields.many2one('res.users', string='Teachers', ondelete='set null', select=True),
                }
 class Student (osv.Model):
     _name = 'nuevo_modulo.student'
@@ -15,12 +18,16 @@ class Student (osv.Model):
         'name': fields.char('Name', size=128, required=True, select=True),
         'code': fields.char('Code', size=32, required=True, select=True),
         'birthday':fields.date('Birthday'),
+        'course_ids': fields.many2many('nuevo_modulo.course', string="Courses"),
         }
 class CourseSession (osv.Model):
     _name = 'nuevo_modulo.session'
     _columns = {
         'subject': fields.char('Subject', size=256, required=True, select=True),
-        'start_time': fields.char('Start time', required=True),
-        'end_time': fields.char('End time'),
+        'start_time': fields.datetime('Start time', required=True),
+        'end_time': fields.datetime('End time'),
+        'course_id': fields.many2one('nuevo_modulo.course', string='Course', required=True, select=True, ondelete='cascade'),
         }
+    _rec_name ='subject'
+    _order = 'start_time'
  
