30224180adfa4a90d01eacef0ef793658b5b4ddd
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2014-08-26 14:39:30 -0600

    Nuevas clases session y student, con sus vistas.

--- cv_clearcorp/cv_clearcorp.py
+++ cv_clearcorp/cv_clearcorp.py
@@ -7,6 +7,21 @@ class Course (osv.Model):
     _columns = {
         'name': fields.char('Name', size=128, required=True, select=True),
         'code': fields.char('Code', size=32, required=True, select=True),
-        'description': fields.text('Description')
+        'description': fields.text('Description'),
         }
-    
+
+class Student (osv.Model):
+    _name = 'cv_clearcorp.student'
+    _columns = {
+        'name': fields.char('Name', size=128, required=True, select=True),
+        'code': fields.char('Code', size=32, required=True, select=True),
+        'birthday': fields.date('Birthday'),
+        }
+
+class CourseSession (osv.Model):
+    _name = 'cv_clearcorp.course.session'
+    _columns = {
+        'subject': fields.char('Subject', size=256, required=True, select=True),
+        'start_time': fields.datetime('Start time', required=True),
+        'end_time': fields.datetime('End time'),
+        }
