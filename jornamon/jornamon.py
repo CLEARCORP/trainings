a00de2ba1bb83b16b2c9de3ddd3b941627246bf5
Jorge Nadal <jorge.nadal@gmail.com>
2014-08-26 17:25:33 -0600

    Added teacher to courses (system user)

--- jornamon/jornamon.py
+++ jornamon/jornamon.py
@@ -8,7 +8,10 @@ class Course (osv.Model):
     _columns = {
         'name' : fields.char('Name',size=128,required=True,select=True),
         'code' : fields.char('Code',size=32,required=True,select=True),
-        'description' : fields.text('Description')
+        'description' : fields.text('Description'),
+        'session_ids' : fields.one2many('jornamon.course.session', 'course_id'),
+        'student_ids' : fields.many2many('jornamon.student', string='Students'),
+        'teacher_id' : fields.many2one('res.users', string='Teacher', ondelete='set null', select=True),
         }
     
 class Student (osv.Model):
@@ -17,14 +20,18 @@ class Student (osv.Model):
     _columns = {
         'name' : fields.char('Name',size=128, required=True, select=True),
         'code' : fields.char('Code',size=32,required=True,select=True),
-        'birthday' : fields.date('Birthday'),          
+        'birthday' : fields.date('Birthday'),
+        'course_ids' : fields.many2many('jornamon.course', string='Courses')          
         }
     
 class CourseSession (osv.Model):   
-    _name = "jornamon.course.session"
+    _name = 'jornamon.course.session'
      
     _columns = {
         'subject' : fields.char('Subject', size=256, required=True, select=True),
         'start_time' : fields.datetime('Start time', required=True),
         'end_time' : fields.datetime('End time'),
+        'course_id' : fields.many2one('jornamon.course', string='Course', required=True, select=True, ondelete='cascade'),
         }
+    _rec_name = 'subject'
+    _order = 'start_time'
