756dd9bd42bb4401843e673a70ae390c89a78006
Diego Alfaro <lda.190@gmail.com>
2014-08-25 18:35:51 -0600

    Version incial del modulo diego-dev, no hace nada por ahora, pero si aparece en la lista

---
+++ diego-dev/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name':'eLearning Managment',
+    'version' : '1.1',
+    'author' : 'Diego Alfaro',
+    'category' : 'Education',
+    'description': """
+eLearning Managment
+===================
+
+    *Courses
+    *Students
+    *Classes
+    *Teachers
+    """,
+    'images':[],
+    'depends':[],
+    'data':[],
+    'demo':[],
+    'test':[],
+    'installable': True,
+    'auto_install': False,
+    
+}  
