d53f1f70680fef812e1cc35233691dfc67e763b0
Oscar Gutierrez L <ogutierrez@transcomer.com>
2014-08-25 18:36:20 -0600

    Version inicial del modulo nuevo-modulo

---
+++ nuevo-modulo/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'eLearning Management',
+    'version': '1.0',
+    'author': 'Oscar Gutierrez',
+    'category': 'Education',
+    'description': '''
+eLearning Management
+====================
+This app is used to manage 
+   
+   -Courses
+   -Students
+   -Teachers
+   -Classes
+''',
+    'images': [],
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,  
+}
