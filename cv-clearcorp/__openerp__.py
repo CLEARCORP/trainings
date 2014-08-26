2f2154b20539f500d0ad5a8209556c8ddae8a16d
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2014-08-25 18:35:39 -0600

    Versión inicial del módulo cv-clearcorp, no hace nada por ahora, pero sí aparece en la lista.

---
+++ cv-clearcorp/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'eLearning Management',
+    'version' : '1.0',
+    'author' : 'Carlos Vásquez',
+    'category' : 'Education',
+    'description': """
+eLearning Management
+====================
+
+    * Courses
+    * Students
+    * Classes
+    * Teachers
+    """,
+    'images': [],
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
