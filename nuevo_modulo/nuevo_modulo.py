e2b1e457175c9725364ece8d998a1a3b0d788f76
Oscar Gutierrez L <ogutierrez@transcomer.com>
2014-08-26 15:05:50 -0600

    SE incluyen Students y Sessions

--- nuevo_modulo/nuevo_modulo.py
+++ nuevo_modulo/nuevo_modulo.py
@@ -5,7 +5,22 @@ from openerp.osv import osv, fields
 class Course (osv.Model):
     _name = 'nuevo_modulo.course'
     _columns = {
-        'name': fields.char('Name',size=128,required=True,select=True),
-        'code': fields.char('Code',size=32,required=True,select=True),
-        'description': fields.text('Description')
+        'name': fields.char('Name', size=128, required=True, select=True),
+        'code': fields.char('Code', size=32, required=True, select=True),
+        'description': fields.text('Description'),
                }
+class Student (osv.Model):
+    _name = 'nuevo_modulo.student'
+    _columns = {
+        'name': fields.char('Name', size=128, required=True, select=True),
+        'code': fields.char('Code', size=32, required=True, select=True),
+        'birthday':fields.date('Birthday'),
+        }
+class CourseSession (osv.Model):
+    _name = 'nuevo_modulo.session'
+    _columns = {
+        'subject': fields.char('Subject', size=256, required=True, select=True),
+        'start_time': fields.char('Start time', required=True),
+        'end_time': fields.char('End time'),
+        }
+ 
