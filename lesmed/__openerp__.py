a361aa5038fe4d1e25d409d0d6de78fc0a20cccf
Lesmed Gutiérrez <lesmedgutiar.16@gmail.com>
2014-08-25 18:40:30 -0600

    Primera version de eLearning, Lesmed Gutiérrez.xD

---
+++ lesmed/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'eLearning Management',
+    'version' : '1.0',
+    'author' : 'Lesmed Gutiérrez',
+    'category' : 'Education',
+    'description' : """ 
+eLearning Management
+=====================
+
+    *Courses
+    *Students
+    *Classes
+    *Teachers
+    """, 
+    'images' : [],
+    'depends' : [],
+    'data' : [],
+    'demo' : [],
+    'test' : [],
+    'installable' : True,
+    'auto_install' : False ,
+
+}
+
+
