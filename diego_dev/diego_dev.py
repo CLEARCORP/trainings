438e2795c5b185a4bfc10bacd2ff3ec7aff6421c
Diego Alfaro <lda.190@gmail.com>
2014-08-26 17:25:59 -0600

    Agregador el teacher

--- diego_dev/diego_dev.py
+++ diego_dev/diego_dev.py
@@ -8,7 +8,9 @@ class Course (osv.Model):
         'name': fields.char('Name', size=128, required=True, select=True),
         'code': fields.char('Code', size=32, required=True, select=True),
         'description': fields.text('Description'),   
-        'session_ids': fields.one2many('diego_dev.course.session','course_id', string='Session'),             
+        'session_ids': fields.one2many('diego_dev.course.session','course_id', string='Session'),   
+        'student_ids': fields.many2many('diego_dev.student', string='Students'), 
+        'teacher_id' : fields.many2one('res.users', string='Teacher', ondelete='set null', select=True)       
                 }
     
 class Student (osv.Model):
@@ -16,7 +18,8 @@ class Student (osv.Model):
     _columns = {
         'name': fields.char('Name', size=128, required=True, select=True),
         'code': fields.char('Code', size=32, required=True, select=True),
-        'birthday': fields.date ('Birthday'),                
+        'birthday': fields.date ('Birthday'), 
+        'course_ids': fields.many2many('diego_dev.course', string='Courses')               
                 }
     
 class CourseSession(osv.Model):
@@ -25,5 +28,7 @@ class CourseSession(osv.Model):
         'subject': fields.char('Subject', size=128, required=True, select=True),
         'start_time': fields.datetime('Start Time', required=True),
         'end_time': fields.datetime('End Time'),
-        'course_id':fields.many2one('diego_dev.course', string='Course', required=True, select=True),
+        'course_id':fields.many2one('diego_dev.course', string='Course', required=True, select=True, ondelete='cascade'),
                 }
+    _rec_name = 'subject'
+    _order = 'start_time'
