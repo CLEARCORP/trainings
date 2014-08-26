aa0814126608ea1e8d2175cf46e164c42a85817c
Jorge Nadal <jorge.nadal@gmail.com>
2014-08-26 15:06:17 -0600

    New classes views actions and menu items for Students and CourseSessions

--- jornamon/jornamon.py
+++ jornamon/jornamon.py
@@ -10,3 +10,21 @@ class Course (osv.Model):
         'code' : fields.char('Code',size=32,required=True,select=True),
         'description' : fields.text('Description')
         }
+    
+class Student (osv.Model):
+    _name = "jornamon.student"
+    
+    _columns = {
+        'name' : fields.char('Name',size=128, required=True, select=True),
+        'code' : fields.char('Code',size=32,required=True,select=True),
+        'birthday' : fields.date('Birthday'),          
+        }
+    
+class CourseSession (osv.Model):   
+    _name = "jornamon.course.session"
+     
+    _columns = {
+        'subject' : fields.char('Subject', size=256, required=True, select=True),
+        'start_time' : fields.datetime('Start time', required=True),
+        'end_time' : fields.datetime('End time'),
+        }
