c29e4111a8c28955428865a02f5a1c08d6ad83b0
Jorge Nadal <jorge.nadal@gmail.com>
2014-08-25 18:35:46 -0600

    Versión inicial del módulo jornamon. No hace nada, pero ya carga correctamente en la lista de módulos.

---
+++ jornamon/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'eLearning Jornamon',
+    'version' : '1.0',
+    'author' : 'Jorge Nadal',
+    'category' : 'Education',
+    'description' : """
+eLearning Jornamon
+==================
+
+This app is only for testing purposes. Most liely it will never do something usefull.
+    * Red
+    * Green
+    * Yellow
+    
+    """,
+    'website' : 'http://google.com',
+    'images' : [],
+    'depends' : [],
+    'data' : [],
+    'qweb' : [],
+    'demo' : [],
+    'test' : [],
+    'installable' : True,
+    'auto_install' : False,  
+
+}
