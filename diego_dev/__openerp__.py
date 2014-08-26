1014396abf23261fc5ba54314878b0bdb5120424
Diego Alfaro <lda.190@gmail.com>
2014-08-26 11:50:12 -0600

    Nuevo objetos courses, ya hay un menu y una accion para la vista

---
+++ diego_dev/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name':'eLearning Managment',
+    'version' : '1.1',
+    'author' : 'Diego Alfaro',
+    'category' : 'Education',
+    'description': """
+eLearning Managment
+===================
+
+    *Courses
+    *Students
+    *Classes
+    *Teachers
+    """,
+    'images':[],
+    'depends':[],
+    'data':[
+        'diego_dev_view.xml',
+        'diego_dev_menu.xml',
+            ],
+    'demo':[],
+    'test':[],
+    'installable': True,
+    'auto_install': False,
+    
+}  
