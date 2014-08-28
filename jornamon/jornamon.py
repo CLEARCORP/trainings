b56f00a8ff99f2b05cef4aac6faca75195f949e2
Jorge Nadal <jorge.nadal@gmail.com>
2014-08-27 18:56:00 -0600

    Todo bien

--- jornamon/jornamon.py
+++ jornamon/jornamon.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from openerp import api
 from openerp.osv import osv, fields
 
 class Course (osv.Model):
     _name = 'jornamon.course'
     
-    
-    def get_seats (self, cr, uid, ids, field_name, arg, context={}):
+    @api.multi
+    def get_seats (self, field_name, arg):
         res = {}
-        courses = self.browse(cr, uid, ids, context=context)
-        for course in courses:
+        
+        for course in self:
             total_seats = course.total_seats
             students = course.student_ids
             occupied_seats = len(students)
@@ -43,7 +44,23 @@ class Course (osv.Model):
         'teacher_id': (lambda self, cr, uid, ids, context={}: uid),
         'total_seats': 20,
         }
-
+    
+    _sql_constraints = [
+        ('total_seats_positive', 'CHECK(total_seats >=0)', 'Total seats must be a positive number'),
+        ]
+    
+    @api.multi
+    def check_seats (self):
+        for course in self:
+            if len(course.student_ids) > course.total_seats:
+                return False
+        return True
+             
+    _constraints = [
+        (check_seats, 'The course has more student than seats, you cannot add more students.', ['student_ids']),   
+        (check_seats, 'You cannot set a number of seats smaller than the number of registered students.', ['total_seats']),      
+        ]
+    
 class Partner (osv.Model):
     _name = 'res.partner'
     _inherit = 'res.partner'
@@ -55,6 +72,19 @@ class Partner (osv.Model):
         'student_code' : fields.char('Student Code',size=32),        
         }
     
+    
+    def on_change_is_company (self, cr, uid, id, is_company, student, context={}):
+        
+        # Llamamos primero al método original que tenía este campo en onchange y que estamos sobreescribiendo.
+        # Ahora en el diccionario de resultado AÑADIMOS nuestro resultado.
+        print "**** Estamos en on_change_is_company"
+        res= self.onchange_type (cr, uid, id, is_company, context=context)
+        # res = {'value': {}, 'domain': {}, 'warning': {}}
+        if is_company and student:
+            res['value'].update({'student': False})
+            res['warning'].update({'title': "Companies cannot be students",
+                           'message': "You changed the partner type to company and it was a student. We have remove the student check"}) 
+        return res
     
        
 class Student (osv.Model):
@@ -69,12 +99,56 @@ class Student (osv.Model):
     
 class CourseSession (osv.Model):   
     _name = 'jornamon.course.session'
-     
+    
+    #Versión de la función como se haría en v7
+    """
+    def button_approve (self, cr, uid, ids, context={}):
+        self.write(cr, uid, ids, {'state': 'pending'}, context=context)
+        return True
+    """
+    
+    #Versión de la función como se haría en v8
+    @api.multi
+    def button_approve (self):
+        #self.write({'state': 'pending'})
+        self.state= 'pending'
+        return True
+    
+    @api.multi
+    def button_start (self):
+        self.state= 'open'
+        return True
+    
+    @api.multi
+    def button_done (self):
+        self.state= 'done'
+        return True
+    
+    @api.multi
+    def button_cancel (self):
+        self.state= 'canceled'
+        return True
+    
+    @api.multi
+    def button_reset (self):
+        self.state= 'draft'
+        return True
+      
     _columns = {
         'subject' : fields.char('Subject', size=256, required=True, select=True),
         'start_time' : fields.datetime('Start time', required=True),
         'end_time' : fields.datetime('End time'),
         'course_id' : fields.many2one('jornamon.course', string='Course', required=True, select=True, ondelete='cascade'),
+        'state': fields.selection([('draft','Draft'),
+                                   ('pending','Pending'),
+                                   ('open','Open'),
+                                   ('done','Done'),
+                                   ('canceled','Canceled')],
+                                  string="State", select=True, required=True),
         }
+    
     _rec_name = 'subject'
     _order = 'start_time'
+    _defaults = {
+                 'state': 'draft',
+                 }
