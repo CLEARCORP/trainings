6f65068bfeb5e5be9701baf98252fff29703cc10
Diego Alfaro <lda.190@gmail.com>
2014-08-29 16:48:52 -0600

    Wizard

--- diego_dev/__init__.py
+++ diego_dev/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*
 
 import diego_dev
+import wizard
--- diego_dev/__openerp__.py
+++ diego_dev/__openerp__.py
@@ -17,10 +17,13 @@ eLearning Managment
     'depends':[],
     'data':[
         'diego_dev_view.xml',
+        'wizard/add_student_to_session.xml',
         'diego_dev_menu.xml',
+        'diego_dev_demo.xml',
+        'diego_dev_workflow.xml',
             ],
     'demo':[
-            'diego_dev_demo.xml'],
+            ],
     'test':[],
     'installable': True,
     'auto_install': False,
